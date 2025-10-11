7be4d640f918eed8e7468b54213258f56824a950
Macbook <MAC@MacBook-Air.local>
2025-10-11 15:46:12 -0700

    Hide top-right icons and media count from cards
    - Commented out the icon and number display in top-right corner
    - Cleaner card appearance without visual clutter
    - Maintains functionality while removing UI elements

--- components/WidgetCard.tsx
+++ components/WidgetCard.tsx
@@ -224,8 +224,8 @@ export default function WidgetCard({ post, aspectRatio = 'square' }: WidgetCardP
               </div>
             </div>
 
-            {/* Top-right icon and media count */}
-            <div className="absolute top-2 right-2 flex gap-2">
+            {/* Top-right icon and media count - Hidden */}
+            {/* <div className="absolute top-2 right-2 flex gap-2">
               {hasMultipleMedia && (
                 <div className="bg-black bg-opacity-60 text-white text-xs px-2 py-1 rounded-full">
                   {allMedia.length}
@@ -238,7 +238,7 @@ export default function WidgetCard({ post, aspectRatio = 'square' }: WidgetCardP
                   <ImageIcon className="w-3 h-3 text-gray-600" />
                 )}
               </div>
-            </div>
+            </div> */}
           </div>
 
         </div>
