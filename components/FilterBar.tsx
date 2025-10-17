8d51f7ca87f193d87d3d2f8bb0beca957b2e10d2
Macbook <MAC@MacBook-Air.local>
2025-10-17 14:53:28 -0700

    Apply animated button style to filter icon button

--- components/FilterBar.tsx
+++ components/FilterBar.tsx
@@ -107,17 +107,43 @@ export default function FilterBar({
         </div>
 
         {/* Filter Toggle Button */}
-        <button
-          onClick={() => setShowFilters(!showFilters)}
-          className={`p-1.5 rounded-md transition-colors flex items-center justify-center ${
-            showFilters 
-              ? 'bg-blue-600 text-white hover:bg-blue-700' 
-              : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
-          }`}
-          title="Filters"
-        >
-          <Filter className="w-4 h-4" />
-        </button>
+        <div className="btn-wrapper">
+          <button 
+            className="btn"
+            onClick={() => setShowFilters(!showFilters)}
+            title="Filters"
+          >
+            <svg className="btn-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
+              ></path>
+            </svg>
+
+            <div className="txt-wrapper">
+              <div className="txt-1">
+                <span className="btn-letter">F</span>
+                <span className="btn-letter">i</span>
+                <span className="btn-letter">l</span>
+                <span className="btn-letter">t</span>
+                <span className="btn-letter">e</span>
+                <span className="btn-letter">r</span>
+              </div>
+              <div className="txt-2">
+                <span className="btn-letter">F</span>
+                <span className="btn-letter">i</span>
+                <span className="btn-letter">l</span>
+                <span className="btn-letter">t</span>
+                <span className="btn-letter">e</span>
+                <span className="btn-letter">r</span>
+                <span className="btn-letter">i</span>
+                <span className="btn-letter">n</span>
+                <span className="btn-letter">g</span>
+              </div>
+            </div>
+          </button>
+        </div>
 
         {/* Clear Filters Button - Always visible when filters are active */}
         {hasActiveFilters && (
